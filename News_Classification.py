e9740dd0445c520d4a8f1eda5a93840bec5c76c4
Atul Kumar <atul1511@iitkgp.ac.in>
2020-05-25 20:49:33 +0530

    Classify News inro Categories

---
+++ News_Classification.py
@@ -0,0 +1,232 @@
+import pandas as pd
+import numpy as np
+from zipfile import ZipFile
+import re
+from nltk.stem.snowball import SnowballStemmer
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer, TfidfTransformer
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score, confusion_matrix, roc_auc_score
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+import string
+from sklearn.linear_model import SGDClassifier
+import logging
+from textblob import TextBlob
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import FeatureUnion
+from sklearn.feature_extraction import DictVectorizer
+
+
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
+
+stop_words_ = set(stopwords.words('english'))
+wn = WordNetLemmatizer()
+
+my_sw = ['make', 'amp',  'news','new' ,'time', 'u','s', 'photos',  'get', 'say']
+def black_txt(token):
+    return  token not in stop_words_ and token not in list(string.punctuation)  and len(token)>2 and token not in my_sw
+  
+  
+def clean_txt(text):
+  clean_text = []
+  clean_text2 = []
+  text = re.sub("'", "",text)
+  text=re.sub("(\\d|\\W)+"," ",text)    
+  clean_text = [ wn.lemmatize(word, pos="v") for word in word_tokenize(text.lower()) if black_txt(word)]
+  clean_text2 = [word for word in clean_text if black_txt(word)]
+  return " ".join(clean_text2)
+
+def polarity_txt(text):
+  return TextBlob(text).sentiment[0]
+
+def subj_txt(text):
+  return  TextBlob(text).sentiment[1]
+
+def len_text(text):
+  if len(text.split())>0:
+         return len(set(clean_txt(text).split()))/ len(text.split())
+  else:
+         return 0
+
+df_news  = pd.read_json(r"C:\Users\Atul Kumar\Downloads\News_Category_Dataset_v2.json", lines = True)
+df_news.category = df_news.category.map(lambda x: "WORDPOST" if x == "THE WORDPOST" else x)
+
+cols = ["authors","link","short_description","date"]
+df_news = df_news.drop(cols,axis=1)
+df_news.rename(columns={'headline': 'text'}, inplace=True)
+
+df_news['polarity'] = df_news['text'].apply(polarity_txt)
+df_news['subjectivity'] = df_news['text'].apply(subj_txt)
+df_news['len'] = df_news['text'].apply(len_text)
+
+class ItemSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, key):
+        self.key = key
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, data_dict):
+        return data_dict[self.key]
+
+
+class TextStats(BaseEstimator, TransformerMixin):
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, data):
+        return [{'pos':  row['polarity'], 'sub': row['subjectivity'],  'len': row['len']} for _, row in data.iterrows()]
+
+pipeline = Pipeline([
+    ('union', FeatureUnion(
+        transformer_list=[
+
+            # Pipeline for pulling features from the text
+            ('text', Pipeline([
+                ('selector', ItemSelector(key='text')),
+                ('tfidf', TfidfVectorizer( min_df =3, max_df=0.2, max_features=None, 
+                    strip_accents='unicode', analyzer='word',token_pattern=r'\w{1,}',
+                    ngram_range=(1, 10), use_idf=1,smooth_idf=1,sublinear_tf=1,
+                    stop_words = None, preprocessor=clean_txt)),
+            ])),
+
+            # Pipeline for pulling metadata features
+            ('stats', Pipeline([
+                ('selector', ItemSelector(key=['polarity', 'subjectivity', 'len'])),
+                ('stats', TextStats()),  # returns a list of dicts
+                ('vect', DictVectorizer()),  # list of dicts -> feature matrix
+            ])),
+
+        ],
+
+        # weight components in FeatureUnion
+        transformer_weights={
+            'text': 0.9,
+            'stats': 1.5,
+        },
+    ))
+])
+
+seed = 40
+X = df_news[['text', 'polarity', 'subjectivity','len']]
+y =df_news['category']
+encoder = LabelEncoder()
+y = encoder.fit_transform(y)
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=seed, stratify=y)
+
+pipeline.fit(x_train)
+
+import spacy
+!python -m spacy download en_core_web_sm
+
+nlp = spacy.load(r"D:\Anaconda\envs\tensorflow\Lib\site-packages\en_core_web_lg\en_core_web_lg-2.2.5")
+
+from keras.preprocessing.text import Tokenizer
+from keras.models import Sequential
+from keras.layers import Activation, Dense, Dropout, LSTM, Embedding
+from sklearn.preprocessing import LabelBinarizer
+from keras.preprocessing.text import Tokenizer, text_to_word_sequence
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import np_utils
+from keras.layers import Dense, Input, LSTM, Bidirectional, Activation, Conv1D, GRU, TimeDistributed
+from keras.layers import Dropout, Embedding, GlobalMaxPooling1D, MaxPooling1D, Add, Flatten, SpatialDropout1D
+from keras.layers import GlobalAveragePooling1D, BatchNormalization, concatenate
+from keras.layers import Reshape, merge, Concatenate, Lambda, Average
+from keras import backend as K
+from keras.engine.topology import Layer
+from keras import initializers, regularizers, constraints
+import time
+
+X = df_news['text']
+y =df_news['category']
+encoder = LabelEncoder()
+y = encoder.fit_transform(y)
+Y = np_utils.to_categorical(y)
+##Create the tf-idf vector
+vectorizer = TfidfVectorizer( min_df =3, max_df=0.2, max_features=None, 
+            strip_accents='unicode', analyzer='word',token_pattern=r'\w{1,}',
+            use_idf=1,smooth_idf=1,sublinear_tf=1,
+            stop_words = None, preprocessor=clean_txt)
+
+seed = 40
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=seed, stratify =y)
+vectorizer.fit(x_train)
+
+word2idx = {word: idx for idx, word in enumerate(vectorizer.get_feature_names())}
+tokenize = vectorizer.build_tokenizer()
+preprocess = vectorizer.build_preprocessor()
+ 
+def to_sequence(tokenizer, preprocessor, index, text):
+    words = tokenizer(preprocessor(text))
+    indexes = [index[word] for word in words if word in index]
+    return indexes
+
+X_train_sequences = [to_sequence(tokenize, preprocess, word2idx, x) for x in x_train]
+print(X_train_sequences[0])
+
+MAX_SEQ_LENGHT=60
+
+N_FEATURES = len(vectorizer.get_feature_names())
+X_train_sequences = pad_sequences(X_train_sequences, maxlen=MAX_SEQ_LENGHT, value=N_FEATURES)
+
+X_test_sequences = [to_sequence(tokenize, preprocess, word2idx, x) for x in x_test]
+X_test_sequences = pad_sequences(X_test_sequences, maxlen=MAX_SEQ_LENGHT, value=N_FEATURES)
+
+EMBEDDINGS_LEN = 300
+
+embeddings_index = np.zeros((len(vectorizer.get_feature_names()) + 1, EMBEDDINGS_LEN))
+for word, idx in word2idx.items():
+    try:
+        embedding = nlp.vocab[word].vector
+        embeddings_index[idx] = embedding
+    except:
+        pass
+      
+print("EMBEDDINGS_LEN=", EMBEDDINGS_LEN)
+
+from keras.models import Model
+from keras.layers import Dense ,LSTM,concatenate,Input,Flatten,BatchNormalization, GRU
+
+text_data = Input(shape=(MAX_SEQ_LENGHT,), name='text')
+meta_data = Input(shape=(3,), name = 'meta')
+x=(Embedding(len(vectorizer.get_feature_names()) + 1,
+                    EMBEDDINGS_LEN,  # Embedding size
+                    weights=[embeddings_index],
+                    input_length=MAX_SEQ_LENGHT,
+                    trainable=False))(text_data)
+x2 = ((LSTM(300, dropout=0.2, recurrent_dropout=0.2)))(x)
+x4 = concatenate([x2, meta_data])
+x5 = Dense(150, activation='relu')(x4)
+x6 = Dropout(0.25)(x5)
+x7 = BatchNormalization()(x6)
+out=(Dense(len(set(y)), activation="softmax"))(x7)
+model = Model(inputs=[text_data, meta_data ], outputs=out)
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(model.summary())
+
+df_cat_train = df_news.iloc[x_train.index][['polarity', 'subjectivity', 'len']]
+df_cat_test = df_news.iloc[x_test.index][['polarity', 'subjectivity', 'len']]
+
+model.fit([X_train_sequences, df_cat_train], y_train, 
+          epochs=12, batch_size=128, verbose=1, 
+          validation_split=0.1)
+ 
+scores = model.evaluate([X_test_sequences, df_cat_test],y_test, verbose=1)
+print("Accuracy:", scores[1])  
+list_result = []
+list_result.append(("LSTM with Multi-Input", scores[1]))
